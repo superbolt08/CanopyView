6d97e63060bc1a20e5c04a7c1b3e8fd0f8dbbe52
superbolt08 <saadtab01@gmail.com>
2024-08-28 11:21:40 -0700

    further intialized server by building a middlewear, dotenv, APIKey, etc

--- index.js
+++ index.js
@@ -1,19 +1,28 @@
 import express from "express"
 import axios from "axios"
 import bodyParser from "body-parser"
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 const port = 3000;
-const APIUrl = "";
+const APIKey = process.env.OPENCAGE_API_KEY;
+const APIBaseUrl = "https://api.opencagedata.com/geocode/v1/json";
 
-app.set('view engine', 'ejs');
-app.use(express.static('public'));
-app.use(bodyParser.urlencoded({extended: true}))
+app.set('view engine', 'ejs'); // Set the view engine to EJS
+app.use(express.static('public')); // helps access css static files
+app.use(bodyParser.urlencoded({extended: true})) // middlewear
 
 app.get("/", (req, res) => {
-    res.render("index.ejs");
+    res.render("index");
 })
 
+app.post("/search", async (req, res) =>{
+    res.render("index");
+})
+
+// start server
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 })

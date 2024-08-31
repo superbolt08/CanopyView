45691b9b817ab8808856b7ca1670640651294306
superbolt08 <saadtab01@gmail.com>
2024-08-31 13:13:50 -0700

    front page WIP, navbar, title, placeholder api elements, footers in the works

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import express from "express"
+import express, { query } from "express"
 import axios from "axios"
 import bodyParser from "body-parser"
 import dotenv from "dotenv";
@@ -12,15 +12,27 @@ const APIBaseUrl = "https://api.opencagedata.com/geocode/v1/json";
 
 app.set('view engine', 'ejs'); // Set the view engine to EJS
 app.use(express.static('public')); // helps access css static files
-app.use(bodyParser.urlencoded({extended: true})) // middlewear
+app.use(bodyParser.urlencoded({extended: true})); // middlewear
 
 app.get("/", (req, res) => {
     res.render("index");
-})
+});
 
 app.post("/search", async (req, res) =>{
-    res.render("index");
-})
+    const location = req.body.location;
+    const APIURL = `${APIBaseUrl}?q=${encodeURIComponent(location)}&key=${APIKey}`;
+    try{
+        const response = await axios.get(APIURL);
+        if(data.results && data.results.length > 0){
+            
+        }
+        res.render("index", {})
+    }
+    catch(error){
+        console.error('Error fetching data from OpenCage API:', error);
+        res.status(500).send('Error fetching data')
+    }
+});
 
 // start server
 app.listen(port, () => {
